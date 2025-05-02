d72009e44fdd88f768c0cf02b643c514a132b8b7
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-02 12:09:00 +0300

    Initial commit of App component with basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 function App() {
+  // State to keep track of the toppings
+  const [toppings, setToppings] = useState(["Cheese"]);
+  const [isPepperoniChecked, setIsPepperoniChecked] = useState(false);
+
+  const handlePepperoniChange = () => {
+    // Toggle the topping when the checkbox is clicked
+    if (isPepperoniChecked) {
+      setToppings(toppings.filter((topping) => topping !== "Pepperoni"));
+    } else {
+      setToppings([...toppings, "Pepperoni"]);
+    }
+    setIsPepperoniChecked(!isPepperoniChecked);
+  };
+
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Customize Your Pizza</h1>
+      <label>
+        <input
+          type="checkbox"
+          checked={isPepperoniChecked}
+          onChange={handlePepperoniChange}
+        />
+        Add Pepperoni
+      </label>
+
+      <h2>Toppings:</h2>
+      <ul>
+        {toppings.map((topping, index) => (
+          <li key={index}>{topping}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
